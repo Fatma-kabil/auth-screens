4e2a40e7a2ef989723f763f9fa2a463f988cd48c
Fatma_kabil <fatmakhaled112@gmail.com>
2024-12-09 10:58:22 +0200

    add title and image on firs screen

--- lib/onboard/login_screen.dart
+++ lib/onboard/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auth_flutter_project/constants.dart';
 import 'package:auth_flutter_project/onboard/widgets/circle_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -6,10 +7,21 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
-      // mainAxisAlignment: MainAxisAlignment.end,
-      children: [CircleWidget()],
-    ));
+          // mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const CircleWidget(),
+            const Text(
+              'Login',
+              style: TextStyle(
+                  fontSize: 30, color: maincolor, fontWeight: FontWeight.bold),
+            ),
+            Image.asset('assets/images/one.jpg'),
+
+            
+          ],
+        ));
   }
 }
